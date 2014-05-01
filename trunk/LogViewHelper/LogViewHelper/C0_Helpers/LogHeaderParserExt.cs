9415b8d960f09fa67dbf4154c5391b7314b7dbf0
shal1928 <shal1928@acc39033-c374-3064-ae63-a805eb15e69c>
2014-05-01 07:22:16 +0000

    Move String Ext to UseAbilities

--- trunk/LogViewHelper/LogViewHelper/C0_Helpers/LogHeaderParserExt.cs
+++ trunk/LogViewHelper/LogViewHelper/C0_Helpers/LogHeaderParserExt.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using LogViewHelper.A0_Models;
+using UseAbilities.Extensions.StringExt;
 
 namespace LogViewHelper.C0_Helpers
 {
     public static class LogHeaderParserExt
-    {   
+    {
         //         10        20        30        40        50        60        70        80        90
         //012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345
         //[30.04.2014 11:34:06,971] INFO  [QueueItemExecutor_OUTBOX_Worker background task processing4709]
@@ -16,7 +17,7 @@ namespace LogViewHelper.C0_Helpers
 
         public static int GetLogMSeconds(this string value, int beginIndex = 21, int endIndex = 23)
         {
-            return int.Parse(value.Copy(beginIndex, endIndex));
+            return value.IsNullOrEmptyOrSpaces() ? 0 : int.Parse(value.Copy(beginIndex, endIndex));
         }
 
         public static LogLevel GetLogLevel(this string value)
@@ -35,50 +36,12 @@ namespace LogViewHelper.C0_Helpers
             value = value.Remove(0, indexOfAnchor + anchor.Length);
             if (removeLast) value = value.RemoveLast();
 
-            return String.IsNullOrEmpty(value) ? 0 : int.Parse(value);
+            return value.IsNullOrEmptyOrSpaces() ? 0 : int.Parse(value);
         }
 
         public static string GetLogMessage(this string value, string anchor = "\n")
         {
             return value.GetAfterNewLine();
         }
-
-
-
-
-
-
-        public static string Copy(this string value, int beginIndex, int endIndex)
-        {
-            return value.Substring(beginIndex, endIndex - beginIndex + 1);
-        }
-
-        public static string RemoveLast(this string value)
-        {
-            return value.Remove(value.IndexOfLast());
-        }
-
-        public static string GetAfterNewLine(this string value)
-        {
-            var newLineIndex = value.IndexOfNext(NEW_LINE);
-            return value.Copy(newLineIndex, value.IndexOfLast());
-        }
-
-        public static int IndexOfNext(this string value, string item, StringComparison comparison = StringComparison.Ordinal)
-        {
-            return value.IndexOf(item, comparison) + 1;
-        }
-
-        public static int IndexOfPreview(this string value, string item, StringComparison comparison = StringComparison.Ordinal)
-        {
-            return value.IndexOf(item, comparison) - 1;
-        }
-
-        public static int IndexOfLast(this string value)
-        {
-            return value.Length - 1;
-        }
-
-        public static readonly string NEW_LINE = "\n";
     }
 }
