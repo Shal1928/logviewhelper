4205a5964093a8513bc12f9f084f92ce0715a949
shal1928 <shal1928@acc39033-c374-3064-ae63-a805eb15e69c>
2014-05-11 13:22:40 +0000

    Add Behavior to DataGrid ContentPresenter (Cell)

---
+++ trunk/LogViewHelper/LogViewHelper/B1_Behaviors/TextHiglightPresenterBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Interactivity;
+using UseAbilities.Extensions.ObjectExt;
+
+namespace LogViewHelper.B1_Behaviors
+{
+    public class TextHiglightPresenterBehavior : Behavior<ContentPresenter>
+    {
+        //MarkerStyle
+        protected override void OnAttached()
+        {
+            //var textBox = AssociatedObject.Content as TextBox;
+            //if(textBox.IsNull()) return;
+
+            AssociatedObject.Initialized += delegate(object sender, EventArgs e)
+            {
+                var a = sender;
+                var b = e;
+                var c = true;
+            };
+        }
+    }
+}
